d1e05d7ae6e40478dc0251d32d67e46906625aa4
Christian <leech92@gmail.com>
2022-02-06 00:41:09 -0500

    create user and session controller

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+
+    attr_reader :password
+
+    validates :email, presence: true, uniqueness: true
+    validates :first_name, :last_name, :password_digest, :session_token, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return user if user && user.is_password?(password)
+        return nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64(16)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+User.destroy_all
+ApplicationRecord.connection.reset_pk_sequence!('users')
+
+demo = User.create!(first_name: "Demo", last_name: "Demo", email: "demo@demo.com", password: "password123")
+user1 = User.create!(first_name: "Derrick", last_name: "Toes", email: "dtoes88@abc.com", password: "password123", brief_bio: "I like to play basketball, but I love to hike.")
+user2 = User.create!(first_name: "Ash", last_name: "Ketchup", email: "aketchup90@abc.com", password: "password123", brief_bio: "Gotta catch 'em all.")
+user3 = User.create!(first_name: "Mike", last_name: "Bison", email: "mbison81@abc.com", password: "password123", brief_bio: "I'm looking for challenging hikes to build up my stamina.")
+user4 = User.create!(first_name: "Hairy", last_name: "Potter", email: "hpotter89@abc.com", password: "password123", brief_bio: "Does anyone know where I can find the locket of Salazar Slytherin?")
+user5 = User.create!(first_name: "Phyllis", last_name: "Dance", email: "pdance80@abc.com", password: "password123", brief_bio: "I like to go on hikes near Scranton, Pennsylvania.")
