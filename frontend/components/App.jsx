b409d0cc58efcb53c10b87fd407aed5db55e6b3c
Christian <leech92@gmail.com>
2022-02-06 02:46:53 -0500

    create splash

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,12 +4,14 @@ import { AuthRoute } from '../util/route_util'
 
 import SignUpFormContainer from './session_form/signup_form_container'
 import LoginFormContainer from './session_form/login_form_container'
+import SplashContainer from './splash/splash_container'
 
 const App = () => (
     <div>
         <Switch>
             <AuthRoute exact path='/login' component={LoginFormContainer} />
             <AuthRoute exact path='/signup' component={SignUpFormContainer} />
+            <Route exact path="/" component={SplashContainer} />
         </Switch>
     </div>
 )
---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+// import SearchBarContainer from "../search/search_bar_container";
+
+const Splash = ({currentUser}) => {
+    return (
+        <div className="splash-container">
+            <div className="splash-search">
+                {(currentUser) ? 
+                    <p className="splash-text">Ready to do this, {currentUser.first_name}?</p> :
+                    <p className="splash-text">Find your next trail</p>}
+                {/* <SearchBarContainer /> */}
+            </div>
+            <div className="splash-info">
+                <div className="splash-info-container">
+                    <p className="splash-info-bold">Empower the next generation of conservationist leaders</p>
+                    <p>We support the Student Conservation Association in uniting young people with hands-on environmentalist challenges.</p>
+                </div>
+                <div className="splash-info-container">
+                    <p className="splash-info-bold">10,000 trees (and counting)</p>
+                    <p>From our birthday to employee anniversaries, we celebrate by giving to One Tree Planted.</p>
+                </div>
+                <div className="splash-info-container">
+                    <p className="splash-info-bold">No trace on the trail</p>
+                    <p>As a Leave No Trace partner, we're commited to keeping outdoor spacs clean, safe, and kind.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Splash
