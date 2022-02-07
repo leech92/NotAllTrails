6be1ce473b464d8402cbd5e29c919a2d25aee48f
Christian <leech92@gmail.com>
2022-02-07 16:10:46 -0500

    create parks/trails model and start aws setup

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,5 @@
 class Park < ApplicationRecord
+    validates :park_name, :description, :acreage, :contact, :country, :state, :latitude, :longitude, presence: true
+
+    has_many :trails
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,7 @@
 class Trail < ApplicationRecord
+    validates :trail_name, :description, :difficulty, :length, :elevation, :duration, :route, :latitude, :longitude, presence: true
+
+    belongs_to :park
+
+    has_one_attached :photo
 end
