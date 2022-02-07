3fc8d0362b49ab143f52381d79327b9b847c773c
Christian <leech92@gmail.com>
2022-02-06 21:21:17 -0500

    create footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import { AuthRoute } from '../util/route_util'
 import SignUpFormContainer from './session_form/signup_form_container'
 import LoginFormContainer from './session_form/login_form_container'
 import SplashContainer from './splash/splash_container'
+import Footer from './footer/footer'
 
 const App = () => (
     <div>
@@ -13,6 +14,7 @@ const App = () => (
             <AuthRoute exact path='/signup' component={SignUpFormContainer} />
             <Route exact path="/" component={SplashContainer} />
         </Switch>
+        <Footer />
     </div>
 )
 
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-left">
+                <Link className="footer-home" to="/"><img className="footer-logo" src={window.logo} /> NotAllTrails</Link>
+            </div>
+            <div className="footer-right">
+                <a href="https://www.linkedin.com/in/christian-lee-371b37160/" target="_blank"><img className="footer-linkedin" src={window.linkedin} /></a>
+                <a href="https://github.com/leech92" target="_blank"><img className="footer-github" src={window.github} /></a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
