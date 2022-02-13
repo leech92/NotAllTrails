cd363a1c9bef2773323116db1d76ef12abe961c5
Christian <leech92@gmail.com>
2022-02-13 04:08:51 -0500

    create stars

---
+++ frontend/components/shared/stars.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component"
+
+class Stars extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            options: this.props.options,
+            onChange: this.props.onChange,
+            rating: this.props.rating
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.rating !== prevProps.rating) {
+            this.setState({rating: this.props.rating})
+        }
+    }
+
+    render() {
+        return(
+            <ReactStars {...this.state.options} onChange={this.state.onChange} value={this.state.rating} />
+        )
+    }
+}
+
+export default Stars
