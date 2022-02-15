2c8fa00784cc2b232242fdeb03678aa3f621943a
Christian <leech92@gmail.com>
2022-02-15 15:58:31 -0500

    ready for heroku push

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -10,6 +10,7 @@ const Footer = () => {
             <div className="footer-right">
                 <a href="https://www.linkedin.com/in/christian-lee-371b37160/" target="_blank"><img className="footer-linkedin" src={window.linkedin} /></a>
                 <a href="https://github.com/leech92" target="_blank"><img className="footer-github" src={window.github} /></a>
+                <a href="https://angel.co/u/christian-lee-17" target="_blank"><img className="footer-angellist" src={window.angellist} /></a>
             </div>
         </div>
     )
